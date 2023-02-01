ef7d35f032aac522efef0ef0080deeae264ee3d6
marcoscode404 <viniciusmg99@outlook.com>
2023-02-01 15:22:31 -0400

    rodando projeto

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{color: '#d60b0b'}}>Modena e Silva</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f8e4e4',
     alignItems: 'center',
     justifyContent: 'center',
   },
