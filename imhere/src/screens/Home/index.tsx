9d23f0b31a77a04f05b734f09c70200a8efd098a
marcoscode404 <viniciusmg99@outlook.com>
2023-02-01 17:05:57 -0400

    trabalhando com input

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,16 +1,22 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do evento
-      </Text>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.eventName}>
+                Nome do evento
+            </Text>
 
-      <Text>
-        Sexta, 4 de Novembro de 2022
-      </Text>
-    </View>
-  )
+            <Text style={styles.eventDate}>
+                Sexta, 4 de Novembro de 2022
+            </Text>
+
+            <TextInput
+                style={styles.input} 
+                placeholder={'Nome do participante'}
+                placeholderTextColor="#6B6B6B"
+            />
+        </View>
+    )
 }
