f9544094045cdcfee5c294cf0d26185255058350
marcoscode404 <viniciusmg99@outlook.com>
2023-02-01 16:54:35 -0400

    Separando codigos em diferentes arquivos

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{color: '#d60b0b'}}>Modena e Silva</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Home />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f8e4e4',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
