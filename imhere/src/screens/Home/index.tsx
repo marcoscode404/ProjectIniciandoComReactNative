578d4f49f5706eac70a8a2576292f8048c288dca
marcoscode404 <viniciusmg99@outlook.com>
2023-02-01 17:53:39 -0400

    Criando components

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,9 +1,10 @@
 import { Text, TextInput, View, TouchableOpacity } from "react-native";
+import { Participant } from "../../components/Participant";
 import { styles } from "./styles";
 
 export function Home() {
 
-    function handleParticipantAdd(){
+    function handleParticipantAdd() {
         console.log("clicou");
     }
 
@@ -17,18 +18,25 @@ export function Home() {
                 Sexta, 4 de Novembro de 2022
             </Text>
 
-            <TextInput
-                style={styles.input}
-                placeholder={'Nome do participante'}
-                placeholderTextColor="#6B6B6B"
-            />
+            {/* formulario */}
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder={'Nome do participante'}
+                    placeholderTextColor="#6B6B6B"
+                />
 
-            {/*  */}
-            <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-                <Text style={styles.buttonText}>
-                    +
-                </Text>
-            </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                    <Text style={styles.buttonText}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+                
+            </View>
+
+            <Participant />
+            <Participant />
+
         </View>
     )
 }
--- imhere/src/screens/Home/styles.ts
+++ imhere/src/screens/Home/styles.ts
@@ -2,29 +2,31 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24
     },
 
     eventName: {
-      color: '#FFF',
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginTop: 48
-    }, 
+        color: '#FFF',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+    },
 
     eventDate: {
         color: '#6B6B6B'
     },
 
     input: {
+        flex: 1,
         height: 56,
         backgroundColor: '#1F1E25',
         borderRadius: 5,
         color: '#FFF',
         padding: 16,
         fontSize: 16,
+        marginRight: 12,
     },
 
     buttonText: {
@@ -39,5 +41,13 @@ export const styles = StyleSheet.create({
         backgroundColor: '#31CF67',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+
+    form: {
+        width: '100%',
+        flexDirection: 'row',
+        marginTop: 36,
+        marginBottom: 42
     }
-  })
+
+})
